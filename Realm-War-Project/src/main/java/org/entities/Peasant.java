fbc94883ec22af5d98ce64450a58a8bccdde363d
alireza <alireza10abasi@gmail.com>
2025-04-25 19:51:03 +0330

    updated classes

--- Realm-War-Project/src/main/java/org/entities/Peasant.java
+++ Realm-War-Project/src/main/java/org/entities/Peasant.java
@@ -1,7 +1,11 @@
 package org.entities;
 
 public class Peasant extends Unit {
-    public Peasant() {
-        super(1, 2, 3, 4, 5, 6, 7);
-    }
+    private final double HIT_POINT = 1;
+    private final double MOVEMENT_RANGE = 2;
+    private final double ATTACK_POWER = 3;
+    private final double ATTACK_RANGE = 4;
+    private final double PAYMENT = 5;
+    private final double RATION = 6;
+    private final int UNIT_SPACE = 7;
 }
--- Realm-War-Project/src/main/java/org/entities/Unit.java
+++ Realm-War-Project/src/main/java/org/entities/Unit.java
@@ -1,23 +1,13 @@
 package org.entities;
 
 public abstract class Unit {
-    private final double HIT_POINT;
-    private final double MOVEMENT_RANGE;
-    private final double ATTACK_POWER;
-    private final double ATTACK_RANGE;
-    private final double PAYMENT;
-    private final double RATION;
-    private final int UNIT_SPACE;
-
-    public Unit(double HIT_POINT, double MOVEMENT_RANGE, double ATTACK_POWER, double ATTACK_RANGE, double PAYMENT, double RATION, int UNIT_SPACE) {
-        this.HIT_POINT = HIT_POINT;
-        this.MOVEMENT_RANGE = MOVEMENT_RANGE;
-        this.ATTACK_POWER = ATTACK_POWER;
-        this.ATTACK_RANGE = ATTACK_RANGE;
-        this.PAYMENT = PAYMENT;
-        this.RATION = RATION;
-        this.UNIT_SPACE = UNIT_SPACE;
-    }
+    private double HIT_POINT;
+    private double MOVEMENT_RANGE;
+    private double ATTACK_POWER;
+    private double ATTACK_RANGE;
+    private double PAYMENT;
+    private double RATION;
+    private int UNIT_SPACE;
 
     public double getHitPoint() {
         return HIT_POINT;
