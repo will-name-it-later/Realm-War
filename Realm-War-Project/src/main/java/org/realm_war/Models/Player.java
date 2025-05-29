62d552aff163626fed88ec2d400c995d068674de
alireza <alireza10abasi@gmail.com>
2025-05-29 18:27:57 +0330

    modified classes

--- Realm-War-Project/src/main/java/org/realm_war/Models/Player.java
+++ Realm-War-Project/src/main/java/org/realm_war/Models/Player.java
@@ -1,4 +1,23 @@
 package org.realm_war.Models;
 
 public class Player {
+    private final String name;
+    private int score;
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
 }
--- Realm-War-Project/src/main/java/org/realm_war/Models/Position.java
+++ Realm-War-Project/src/main/java/org/realm_war/Models/Position.java
@@ -1,4 +1,17 @@
 package org.realm_war.Models;
 
 public class Position {
+    private int x;
+    private int y;
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
