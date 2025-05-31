3db64f99f9a14e978c9fab72d1ab3072c59ce3a9
Mostafa Sadidi <mosisadidi2018@gmail.com>
2025-06-01 00:40:06 +0330

    ResourceLoader done. P

--- Realm-War-Project/src/main/java/org/realm_war/Utilities/ResourceLoader.java
+++ Realm-War-Project/src/main/java/org/realm_war/Utilities/ResourceLoader.java
@@ -1,4 +1,42 @@
 package org.realm_war.Utilities;
 
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Loads images and configuration files from a local folder under src/main/java/org/realm_war/Utilities/assets/
+ */
 public class ResourceLoader {
+
+    private static final Map<String, Image> imageCache = new HashMap<>();
+
+    private static final String BASE_PATH = "src/main/java/org/realm_war/Utilities/assets/";
+
+    public Image loadImage(String filename) {
+        if (imageCache.containsKey(filename)) {
+            return imageCache.get(filename);
+        }
+
+        File file = new File(BASE_PATH + filename);
+        if (!file.exists()) {
+            System.err.println("Image not found: " + file.getPath());
+            return null;
+        }
+
+        try {
+            Image img = ImageIO.read(file);
+            imageCache.put(filename, img);
+            return img;
+        } catch (IOException e) {
+            System.err.println("Failed to read image: " + file.getPath());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void clearCache() {
+        imageCache.clear();
+    }
 }
