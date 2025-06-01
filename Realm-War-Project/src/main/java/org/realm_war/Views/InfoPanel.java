010122bfe1a8d5664be4cfc75b6b6efecd53366e
Mostafa Sadidi <mosisadidi2018@gmail.com>
2025-06-02 00:07:24 +0330

    InfoPanel labels added. P

--- Realm-War-Project/src/main/java/org/realm_war/Views/InfoPanel.java
+++ Realm-War-Project/src/main/java/org/realm_war/Views/InfoPanel.java
@@ -7,5 +7,9 @@ import org.realm_war.Models.GameState;
 import org.realm_war.Models.Realm;
 
 public class InfoPanel extends JPanel{
-
+    private JLabel playerLabel;
+    private JLabel goldLabel;
+    private JLabel foodLabel;
+    private JLabel unitSpaceLabel;
+    private JLabel turnLabel;
 }
