5e60833fbab13c0d43ffe1bafb0e10370b413976
LiGER <samarghandi112w34@gmail.com>
2025-06-02 21:09:30 +0330

    added some colors to Constants class

--- Realm-War-Project/src/main/java/org/realm_war/Utilities/Constants.java
+++ Realm-War-Project/src/main/java/org/realm_war/Utilities/Constants.java
@@ -1,7 +1,12 @@
 package org.realm_war.Utilities;
 
+import java.awt.*;
+
 public class Constants {
     private static final int mapSize = 20;
+    public static final Color clr_1 = new Color(59,53,37);
+    public static final Color clr_2 = new Color(201,196,190);
+    public static final Color clr_3 = new Color(207,204,204);
     public static int getMapSize(){
         return mapSize;
     }
