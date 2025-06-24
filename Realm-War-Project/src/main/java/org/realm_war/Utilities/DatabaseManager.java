c157f2f46c111c2f65925cb1537eb8075a94bfa8
Mostafa Sadidi <mosisadidi2018@gmail.com>
2025-06-24 23:15:32 +0330

    'DatabaseManager' class added. P

---
+++ Realm-War-Project/src/main/java/org/realm_war/Utilities/DatabaseManager.java
@@ -0,0 +1,15 @@
+package org.realm_war.Utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Save_And_Load";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
