4f78d455e103567dd285024a38caa39288e0a96c
Mostafa Sadidi <mosisadidi2018@gmail.com>
2025-07-08 01:29:16 +0330

    Little change in log: added offset to timestamp. P

--- Realm-War-Project/src/main/java/org/realm_war/Utilities/LogEntry.java
+++ Realm-War-Project/src/main/java/org/realm_war/Utilities/LogEntry.java
@@ -1,18 +1,19 @@
 package org.realm_war.Utilities;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class LogEntry {
-    private static final transient SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
     private final String timestamp;
     private final int playerID;
     private final String actionType;
     private final String details;
 
     public LogEntry(int playerID, String actionType, String details) {
-        this.timestamp = sdf.format(new Date()); // Automatically capture and format the current time
+        // Automatically capture the current time using the system's default timezone.
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss (XXX)");
+        this.timestamp = OffsetDateTime.now(ZoneId.systemDefault()).format(formatter);
         this.playerID = playerID;
         this.actionType = actionType;
         this.details = details;
